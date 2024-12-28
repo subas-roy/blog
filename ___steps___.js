c16cc6e5c0081b8d643d0f89e0aaa07716a130ab
subas-roy <subasroy46@gmail.com>
2024-12-29 02:40:48 +0600

    Install express, mongoose, typescript, cors and dotenv

---
+++ ___steps___.js
@@ -0,0 +1,23 @@
+/**
+ * 1. Create a folder with project name and open in vscode
+ * 2. Initialize a "package.json" file in the terminal
+ *    -> npm init -y
+ *
+ * 3. Install express, mongoose, typescript, cors and dotenv
+ *    -> npm install express
+ *    -> npm install mongoose --save
+ *    -> npm install typescript --save-dev
+ *    -> npm i cors
+ *    -> npm i dotenv
+ *
+ * 4. Initialize a "tsconfig.json" file
+ *    -> tsc -init
+ *    -> zUncomment and change: "rootDir": "./" to "rootDir": "./src" and "outDir": "./" to "outDir": "./dist",
+ *
+ * 5. Create "src" folder in the root folder and create "app.ts" file in the folder. Paste the "Hello world" example from express
+ *
+ * 6. Create '"build": "tsc"' in "package.json" file to convert typescript to javascript.
+ *    -> npm run build
+ *    -> Run js file: node ./dist/app.js
+ *
+ */
