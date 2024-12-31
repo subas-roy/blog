136f93af82ef8c048cffc4a4c03bcb5aa16d4364
subas-roy <subasroy46@gmail.com>
2025-01-01 00:52:51 +0600

    Implement validateRequest Middleware

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,9 +1,15 @@
 import exrress from 'express';
 import { userControllers } from './user.controller';
+import { userValidations } from './user.validation';
+import validateRequest from '../../middlewares/validateRequest';
 
 const router = exrress.Router(); // an object
 
 // will call controller func
-router.post('/register', userControllers.createUser);
+router.post(
+  '/register',
+  validateRequest(userValidations.createUserValidationSchema),
+  userControllers.createUser,
+);
 
 export const userRoutes = router;
--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,9 +1,15 @@
 import { z } from 'zod';
 
-const userValidationSchema = z.object({
-  password: z.string().min(6, 'Password must be at least 6 characters long.'),
+const createUserValidationSchema = z.object({
+  body: z.object({
+    name: z.string(),
+    email: z.string().email('Invalid email address.'),
+    password: z.string().min(6, 'Password must be at least 6 characters long.'),
+    role: z.enum(['admin', 'user']).default('user').optional(),
+    isBlocked: z.boolean().default(false).optional(),
+  }),
 });
 
-export const userValidation = {
-  userValidationSchema,
+export const userValidations = {
+  createUserValidationSchema,
 };
