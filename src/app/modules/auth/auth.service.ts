4bb1b1e40d0d7ebea082547850aca5f105a9bec7
subas-roy <subasroy46@gmail.com>
2025-01-12 15:01:47 +0600

    Implement business logic validation in auth service

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -6,11 +6,24 @@ import httpStatus from 'http-status';
 const loginUser = async (payload: TLoginUser) => {
   // checking if the user is exist
   const isUserExists = await User.findOne({ email: payload.email });
-  console.log(isUserExists);
-
   if (!isUserExists) {
     throw new AppError(httpStatus.NOT_FOUND, 'This user is not found');
   }
+
+  // checking if the user is already blocked
+  const isUserBlocked = isUserExists.isBlocked;
+  if (isUserBlocked) {
+    throw new AppError(httpStatus.FORBIDDEN, 'This user is alredy blocked');
+  }
+
+  // checking if the password is correct
+  const isPasswordMatched = isUserExists.password === payload.password;
+  if (!isPasswordMatched) {
+    throw new AppError(httpStatus.FORBIDDEN, 'Password does not matched');
+  }
+
+  // Access granted: send access token & refreshtoken
+
   return {};
 };
 
