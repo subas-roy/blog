21c452e3ade47f501d606277f98b6406b74b291c
subas-roy <subasroy46@gmail.com>
2025-01-12 08:48:37 +0600

    Implement AppError class

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,7 +1,16 @@
+import AppError from '../../errors/AppError';
+import { User } from '../user/user.model';
 import { TLoginUser } from './auth.interface';
+import httpStatus from 'http-status';
 
 const loginUser = async (payload: TLoginUser) => {
-  console.log(payload);
+  // checking if the user is exist
+  const isUserExists = await User.findOne({ email: payload.email });
+  console.log(isUserExists);
+
+  if (!isUserExists) {
+    throw new AppError(httpStatus.NOT_FOUND, 'This user is not found');
+  }
   return {};
 };
 
