3c7f401ba9d9341588e3746b917fc7a983d17d65
subas-roy <subasroy46@gmail.com>
2025-01-19 23:09:22 +0600

    Convert Zod Error

---
+++ src/app/interface/error.ts
@@ -0,0 +1,4 @@
+export type TError = {
+  path: string | number;
+  message: string;
+}[];
--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -2,20 +2,54 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-unused-vars */
 import { NextFunction, Request, Response } from 'express';
+import { ZodError, ZodIssue } from 'zod';
+import config from '../config';
+import { TError } from '../interface/error';
+import handleZodError from '../errors/handleZodError';
 
 const globalErrorHandler = (
-  error: any,
+  err: any,
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
-  const statusCode = error.statusCode || 500;
-  const message = error.message || 'Something went wrong!';
+  // setting default values
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'Something went wrong!';
+
+  let error: TError = [
+    {
+      path: '',
+      message: 'Something went wrong!',
+    },
+  ];
+
+  // is instanceof ZodError
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    error = simplifiedError?.error;
+  }
+
+  // ultimate return
   return res.status(statusCode).json({
     success: false,
     message,
-    error: error,
+    statusCode,
+    error,
+    stack: config.NODE_ENV === 'development' ? err?.stack : null,
+    // err: err,
   });
 };
 
 export default globalErrorHandler;
+
+// Error pattern
+/*
+  success
+  message
+  statusCode
+  error
+  stack
+ */
